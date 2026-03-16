7dbbb30860be29f683126f74ca604939ff439c43
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-03-16 08:04:40 +0545

    add random color palette generator

---
+++ 03.random-palatte/main.js
@@ -0,0 +1,62 @@
+const selectFormat = document.getElementById('format');
+const selectTone = document.getElementById('tone');
+const generateBtn = document.getElementById('generateBtn');
+const paletteContainer = document.getElementById('paletteContainer');
+const palette = document.getElementById('palette');
+
+function randomRGB(tome){
+  let min = 0;
+  let max = 255;
+  if(tome === 'light'){
+    min = 200;
+  }else if(tome === 'dark'){
+    max = 100;
+  }
+  const r = Math.floor(Math.random() * (max - min + 1)) + min;
+  const g = Math.floor(Math.random() * (max - min + 1)) + min;
+  const b = Math.floor(Math.random() * (max - min + 1)) + min;
+  return `rgb(${r},${g},${b})`;
+}
+function convertToHex(rgb){
+  const rgbValues = rgb.match(/\d+/g).map(Number);
+  const hex = rgbValues.map(value => value.toString(16).padStart(2, '0')).join('');
+  return `#${hex}`;
+}
+
+function generatePalette(){
+  palette.innerHTML = '';
+  let color ;
+  for(let i=0; i<5; i++){
+  if(selectFormat.value === 'rgb'){
+    color = randomRGB(selectTone.value);
+  }else{
+    const rgb = randomRGB(selectTone.value);
+    color = convertToHex(rgb);
+  }
+  const colorBox = document.createElement('div');
+  colorBox.classList.add('color-box');
+  colorBox.style.backgroundColor = color;
+  if(selectFormat.value === 'hex' && selectTone.value === 'dark'){
+    colorBox.textContent = color;
+    colorBox.style.color = '#fff';
+
+  }else{
+    colorBox.textContent = color;
+    colorBox.style.color = '#000';
+    colorBox.style.textShadow = '1px 1px 2px rgba(0,0,0,0.5)';
+    colorBox.style.fontSize= '12px';
+  }
+  palette.appendChild(colorBox);
+  colorBox.addEventListener('click',()=>{
+    const color = colorBox.textContent;
+    navigator.clipboard.writeText(color).then(()=>{
+      alert(`Color ${color} copied to clipboard!`);
+    }).catch(err=>{
+      console.error('Failed to copy color: ',err);
+    })
+  })
+
+  }
+}
+generateBtn.addEventListener('click', generatePalette)
+generatePalette();
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Some Projects on DOM
 1. dark mode toggle **Link**: https://darkmodetoggle-ten.vercel.app/
 2. dynamic list generator **Link**: https://dom-ten-zeta.vercel.app/
+
